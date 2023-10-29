9e3464f63ba2df729c73b939015211be3f59e855
jingjie-wan <62317828+jingjie-wan@users.noreply.github.com>
2023-10-29 18:00:17 -0400

    Add files via upload

---
+++ Project2/Proj2_skeleton.py
@@ -0,0 +1,40 @@
+#
+# Name:
+#
+
+from Proj2_tree import printTree
+
+#
+# The following two trees are useful for testing.
+#
+smallTree = \
+    ("Is it bigger than a breadbox?",
+        ("an elephant", None, None),
+        ("a mouse", None, None))
+mediumTree = \
+    ("Is it bigger than a breadbox?",
+        ("Is it gray?",
+            ("an elephant", None, None),
+            ("a tiger", None, None)),
+        ("a mouse", None, None))
+
+def main():
+    """DOCSTRING!"""
+    # Write the "main" function for 20 Questions here.  Although
+    # main() is traditionally placed at the top of a file, it is the
+    # last function you will write.
+
+def simplePlay(tree):
+    """DOCSTRING!"""
+    
+def play(tree):
+    """DOCSTRING!"""
+    
+#
+# The following two-line "magic sequence" must be the last thing in
+# your file.  After you write the main() function, this line it will
+# cause the program to automatically play 20 Questions when you run
+# it.
+#
+if __name__ == '__main__':
+    main()
